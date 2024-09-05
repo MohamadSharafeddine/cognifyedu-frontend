07144a2009a3e1fc0a0eeaf25137e520ea61ea74
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 02:09:02 +0300

    Rearranged the routes in the app for testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Footer from './components/Footer/Footer';
+import Classes from './pages/Classes/Classes';
+import ClassesCard from './components/ClassCard/ClassCard';
 
 function App() {
   return (
@@ -10,10 +12,9 @@ function App() {
       <div className="App">
         <div className="content">
           <Routes>
-            {/* <Route path="/" element={<Sidebar />} /> */}
+            <Route path="/" element={<Classes />} />
           </Routes>
         </div>
-        <Sidebar />
         <Footer />
       </div>
     </Router>
