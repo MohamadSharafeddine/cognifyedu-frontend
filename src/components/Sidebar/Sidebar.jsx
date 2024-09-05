2c34360e90abbcfc51029af4fa7125d4526376a8
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-05 21:32:36 +0300

    Imported fontawesome, router, and logo to sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThList } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/Logos/Logo_bar.png';
+
+const Sidebar = ({ menuItems = ['Classes', 'Class1', 'Class2'] }) => {
+  return (
+    <div className="sidebar">
+    </div>
+  );
+};
+
+export default Sidebar;
