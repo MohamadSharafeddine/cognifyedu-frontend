3781a02941612f4567f06a06fef74c5993b4c2a7
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-05 19:38:36 +0300

    Created footer content and classnames

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,34 @@ import Logo from '../../assets/Logos/Logo.png';
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="left">
+        <img src={Logo} alt="CognifyEdu Logo" />
+        <div className="social-icons">
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faXTwitter} className="social-icon" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faYoutube} className="social-icon" />
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+          </a>
+        </div>
+        <div className="location">Lebanon - English</div>
+      </div>
+      <div className="center">
+        <div>Support</div>
+        <a href="#">FAQ</a>
+        <div className="copyright">© 2024 CognifyEdu</div>
+      </div>
+      <div className="right">
+        <div>About</div>
+        <a href="#">Careers</a>
+        <a href="#">Terms & Conditions</a>
+      </div>
     </div>
   );
 };
