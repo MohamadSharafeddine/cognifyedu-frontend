0e395eae554afa276c0c8c66da46ed61c639454b
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 01:38:52 +0300

    Adjusted logo path in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css'; // Importing the CSS file
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXTwitter, faInstagram, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import Logo from '../../assets/Logos/Logo.png';
+import Logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
