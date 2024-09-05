126b9d840d86ed72e3bfd998d750a0828b96fdba
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-05 19:36:23 +0300

    Imported fontawesome needed icons to footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'; // Importing the CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faInstagram, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Logo from '../../assets/Logos/Logo.png';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+    </div>
+  );
+};
+
+export default Footer;
