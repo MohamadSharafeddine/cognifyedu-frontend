3201ee92c1e323df9fd536c9ba58ab5249788ebf
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-05 21:51:16 +0300

    Added sidebar and footer to app for layout tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <div className="content">
+          <Routes>
+            {/* <Route path="/" element={<Sidebar />} /> */}
+          </Routes>
+        </div>
         <Sidebar />
+        <Footer />
       </div>
     </Router>
   );
