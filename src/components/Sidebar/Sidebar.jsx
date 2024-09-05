6cf33bb475effe252fa0b46761758fccfe8323d2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-05 21:33:21 +0300

    Added logo and its route

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,6 +8,9 @@ import Logo from '../../assets/Logos/Logo_bar.png';
 const Sidebar = ({ menuItems = ['Classes', 'Class1', 'Class2'] }) => {
   return (
     <div className="sidebar">
+      <Link to="/classes">
+        <img src={Logo} alt="CognifyEdu Logo" />
+      </Link>
     </div>
   );
 };
