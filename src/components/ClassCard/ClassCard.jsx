e143c84e3224bc8d86341b6581dc10c45758b5c1
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 18:43:09 +0300

    Added useDispatch hook to class card

--- src/components/ClassCard/ClassCard.jsx
+++ src/components/ClassCard/ClassCard.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import './ClassCard.css';
-import { useDispatch } from 'react-redux';
-import { selectClass } from '../../redux/actions/classActions';
 
 const ClassCard = ({
   className = 'Class Name',
   teacherName = 'Teacher Name',
   description = 'This is a default description for the class. It provides some basic information about the class.',
 }) => {
-  const dispatch = useDispatch();
-
   return (
-    <div
-      className="class-card"
-      onClick={() => dispatch(selectClass(className))}
-    >
+    <div className="class-card">
       <div className="class-card-header">
         <span className="classname">{className}</span>
         <span className="teacher">{teacherName}</span>
