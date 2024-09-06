6c7c5bacf75120770ea95c20d9fb49724310dc18
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-07 01:40:18 +0300

    Created AddClassTeacher component

---
+++ src/components/AddClassTeacher/AddClassTeacher.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './AddClassTeacher.css';
+import Button from '../Button/Button';
+
+const AddClassTeacher = ({ onClose, onAddClass }) => {
+  return (
+    <div className="modal">
+    </div>
+  );
+};
+
+export default AddClassTeacher;
