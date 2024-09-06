17fc1c82d2a73816ce4cbb2a8b47052976ed7cf2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 20:52:41 +0300

    Created studentsData array and filled it with dummy data for testing

--- src/components/Students/Students.jsx
+++ src/components/Students/Students.jsx
@@ -4,6 +4,15 @@ import defaultAvatar from '../../assets/profile.png';
 import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 
+const studentsData = [
+  { id: 1, name: 'Miriam Wilderman', avatar: 'path_to_avatar1.png' },
+  { id: 2, name: 'Betsy Zboncak', avatar: 'path_to_avatar2.png' },
+  { id: 3, name: 'Dean Senger', avatar: 'path_to_avatar3.png' },
+  { id: 4, name: 'Katie Hackett', avatar: 'path_to_avatar4.png' },
+  { id: 5, name: 'Seth Erdman', avatar: 'path_to_avatar5.png' },
+  { id: 6, name: 'Priscilla Bradtke', avatar: 'path_to_avatar6.png' },
+];
+
 const Students = ({ searchTerm }) => {
   const navigate = useNavigate();
   return (
