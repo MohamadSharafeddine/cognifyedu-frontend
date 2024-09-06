a3b040b5961fb53834d0edf49174b70f1c9c0127
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 21:12:25 +0300

    Created Marks component

---
+++ src/components/Marks/Marks.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Marks.css";
+import defaultAvatar from "../../assets/profile.png";
+
+const Marks = ({ searchTerm }) => {
+
+  return (
+    <div className="Marks-list">
+    </div>
+  );
+};
+
+export default Marks;
