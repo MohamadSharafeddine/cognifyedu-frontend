8d15dcc47e023a7db406f2989ac56274a579e80a
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-07 01:54:56 +0300

    Adjusted classReducer to add classes

--- src/redux/reducers/classReducer.js
+++ src/redux/reducers/classReducer.js
@@ -1,19 +1,32 @@
 import { SELECT_CLASS } from '../actions/classActions';
 
 const initialState = {
-  selectedClass: 'Classes',
+  selectedClass: "Classes",
+  classes: [
+    { className: "Class1", teacherName: "Max Fischer", description: "Description for Class1" },
+    { className: "Class2", teacherName: "Jane Doe", description: "Description for Class2" },
+    { className: "Class3", teacherName: "John Smith", description: "Description for Class3" },
+  ],
 };
 
-const classReducer = (state = initialState, action) => {
+export const classReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SELECT_CLASS:
+    case "SELECT_CLASS":
       return {
         ...state,
         selectedClass: action.payload,
       };
+
+    case "ADD_CLASS":
+      return {
+        ...state,
+        classes: [...state.classes, action.payload],
+      };
+
     default:
       return state;
   }
 };
+
 
 export default classReducer;
