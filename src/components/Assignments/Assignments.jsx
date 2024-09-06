db663079d303b187b524ccf990226acbe1b24836
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 20:29:40 +0300

    Added dummy data for testing

--- src/components/Assignments/Assignments.jsx
+++ src/components/Assignments/Assignments.jsx
@@ -3,6 +3,20 @@ import './Assignments.css';
 import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
 
+const assignmentsData = [
+  { title: 'Project', dueDate: '2024-10-10' },
+  { title: 'Assignment', dueDate: '2024-10-11' },
+  { title: 'Math test', dueDate: '2024-10-12' },
+  { title: 'English test', dueDate: '2024-10-13' },
+  { title: 'Science test', dueDate: '2024-10-14' },
+  { title: 'History test', dueDate: '2024-10-15' },
+  { title: 'Geography test', dueDate: '2024-10-16' },
+  { title: 'Art test', dueDate: '2024-10-17' },
+  { title: 'Music test', dueDate: '2024-10-18' },
+  { title: 'PE test', dueDate: '2024-10-19' },
+  { title: 'Social Studies test', dueDate: '2024-10-20' },
+];
+
 const Assignments = ({ searchTerm }) => {
   return (
     <div className="assignments-list">
