73a52b648c7bc7df5a034a1ad6a134fcbdc75bce
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 20:29:08 +0300

    Created assignments component

---
+++ src/components/Assignments/Assignments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Assignments.css';
+import Button from '../Button/Button';
+import { useNavigate } from 'react-router-dom';
+
+const Assignments = ({ searchTerm }) => {
+  return (
+    <div className="assignments-list">
+    </div>
+  );
+};
+
+export default Assignments;
