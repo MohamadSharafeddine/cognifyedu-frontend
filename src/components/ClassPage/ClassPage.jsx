e153eedd8eff70dad934e5a35cea6316f059c466
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 20:12:28 +0300

    Restored ClassPage data

--- src/components/ClassPage/ClassPage.jsx
+++ src/components/ClassPage/ClassPage.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import './ClassPage.css';
+import React, { useState } from "react";
+import "./ClassPage.css";
+import Assignments from "../../components/Assignments/Assignments";
+import Students from "../../components/Students/Students";
+import Marks from "../../components/Marks/Marks";
+import Button from "../Button/Button";
+import TabBar from "../TabBar/TabBar";
 
-const ClassPage = ({ className }) => {
+const ClassPage = () => {
+  const [activeTab, setActiveTab] = useState("Classwork");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleClick = () => {
+    console.log("Add button clicked");
+    if (activeTab === "Classwork") {
+      console.log("Classwork tab clicked");
+    } else if (activeTab === "Students") {
+      console.log("Students tab clicked");
+    }
+  };
+
+  const tabs = ["Classwork", "Students", "Marks"];
+
   return (
     <div className="class-page">
-      <h2>{className} Details</h2>
-      <p>Welcome to {className}! Here are the details and classwork.</p>
+      <div className="header">
+        <TabBar tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+
+        <div className="search-and-add">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          {activeTab !== "Marks" && (
+            <Button
+              color="#25738b"
+              text="Add"
+              size="medium"
+              onClick={handleClick}
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="content">
+        {activeTab === "Classwork" && <Assignments searchTerm={searchTerm} />}
+        {activeTab === "Students" && <Students searchTerm={searchTerm} />}
+        {activeTab === "Marks" && <Marks searchTerm={searchTerm} />}
+      </div>
     </div>
   );
 };
