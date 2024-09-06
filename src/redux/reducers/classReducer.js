3efbad6605fb752b85934e3a439fa67b2cf67385
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-07 02:05:25 +0300

    Reset initialState in classReducer

--- src/redux/reducers/classReducer.js
+++ src/redux/reducers/classReducer.js
@@ -1,32 +1,19 @@
 import { SELECT_CLASS } from '../actions/classActions';
 
 const initialState = {
-  selectedClass: "Classes",
-  classes: [
-    { className: "Class1", teacherName: "Max Fischer", description: "Description for Class1" },
-    { className: "Class2", teacherName: "Jane Doe", description: "Description for Class2" },
-    { className: "Class3", teacherName: "John Smith", description: "Description for Class3" },
-  ],
+  selectedClass: 'Classes',
 };
 
-export const classReducer = (state = initialState, action) => {
+const classReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SELECT_CLASS":
+    case SELECT_CLASS:
       return {
         ...state,
         selectedClass: action.payload,
       };
-
-    case "ADD_CLASS":
-      return {
-        ...state,
-        classes: [...state.classes, action.payload],
-      };
-
     default:
       return state;
   }
 };
-
 
 export default classReducer;
