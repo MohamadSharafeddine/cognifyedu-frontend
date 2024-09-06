6c92d49ac0437fe7ec8ce28d216740b4aaa5add8
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-06 18:48:14 +0300

    Created TabBar component

---
+++ src/components/TabBar/TabBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TabBar.css";
+const TabBar = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <div className="tab-bar">
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          className={activeTab === tab ? "active" : ""}
+          onClick={() => setActiveTab(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TabBar;
