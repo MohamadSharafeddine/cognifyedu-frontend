70b3bb80a3cf777fb8241cacd1e95d5a257cce93
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-08 05:53:15 +0300

    Edited index upon redux changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import { Provider } from 'react-redux'; // Redux Provider
-import store from './redux/store';      // Redux store
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
