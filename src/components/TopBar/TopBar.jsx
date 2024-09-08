55fb2bcfddd5f3fc4ae0af1d4240361c661fc355
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-08 05:44:18 +0300

    Edited imports for TopBar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './TopBar.css';
 import userProfile from '../../assets/profile.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const TopBar = () => {
   return (
