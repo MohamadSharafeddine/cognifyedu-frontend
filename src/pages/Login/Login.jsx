7ec4c989b81b64cf20ca58f2766f130f7179ae7e
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-09 23:15:59 +0300

    Created Login form

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import './Login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Button from '../../components/Button/Button';
+import logoTitle from '../../assets/logo-title.png';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = () => {
+    console.log('Login Data:', formData);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="hero-section">
+        <img src={logoTitle} alt="CognifyEdu Logo" />
+        <p>Unlocking Potential, One Insight at a Time.</p>
+      </div>
+      <div className="login-form-container">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group password-group">
+          <label>Password</label>
+          <div className="password-wrapper">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder="Enter your Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon
+              icon={showPassword ? faEyeSlash : faEye}
+              className="password-icon"
+              onClick={togglePasswordVisibility}
+              style={{ color: '#25738b' }}
+            />
+          </div>
+        </div>
+        <div className="form-actions">
+          <Button text="Login" onClick={handleLogin} color="#25738b" size="medium" />
+        </div>
+        <p className="register-link">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
