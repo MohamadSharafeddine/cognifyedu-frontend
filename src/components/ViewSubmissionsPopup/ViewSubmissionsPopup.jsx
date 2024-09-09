58b9fcdf3507778e3d899d0d7b39fd7b06ac3565
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-09 23:58:34 +0300

    Created viewsubmissionspopup and added the necessary imports

---
+++ src/components/ViewSubmissionsPopup/ViewSubmissionsPopup.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './ViewSubmissionsPopup.css';
+import Button from '../Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import { fetchSubmissions } from '../../actions/submissionsActions';
+
+const ViewSubmissionsPopup = ({ onClose, assignmentTitle }) => {
+  return (
+    <div className="modal-backdrop">
+    </div>
+  );
+};
+
+export default ViewSubmissionsPopup;
