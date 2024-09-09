2504d3bd0fd5a0529917dcbcc9c7b9beb8f4b058
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-09 23:13:38 +0300

    Imported fontawesome and necessary components to Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import './Register.css'
+import React, { useState } from 'react';
+import './Register.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Button from '../../components/Button/Button';
+import logoTitle from '../../assets/logo-title.png';
+import { Link } from 'react-router-dom';
+
 const Register = () => {
   return (
     <div>Register</div>
