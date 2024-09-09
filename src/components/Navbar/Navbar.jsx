cefc20effc6b9c31595ab9668ba67f23c086859c
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-09 23:37:33 +0300

    Imported hashlink and dom and logo into navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/logo-bar.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    </nav>
+  );
+};
+
+export default Navbar;
