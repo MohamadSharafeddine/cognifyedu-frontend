2528da6986032f83e63272d9bc1e809714698057
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-09 23:14:44 +0300

    Added functions to toggle password visibility, and handle register data

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,6 +7,28 @@ import logoTitle from '../../assets/logo-title.png';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = () => {
+    console.log('Register Data:', formData);
+  };
+
   return (
     <div>Register</div>
   )
