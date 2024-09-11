eebc7ebb27f4fabbf0bf41264a2cf26e981baf4b
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-11 22:16:36 +0300

    Added a thunk to add a new assignment

--- src/redux/slices/assignmentsSlice.js
+++ src/redux/slices/assignmentsSlice.js
@@ -14,3 +14,27 @@ export const fetchAssignmentsByCourse = createAsyncThunk(
     }
   }
 );
+
+export const addAssignment = createAsyncThunk(
+  "assignments/add",
+  async (assignmentData, { rejectWithValue }) => {
+    try {
+      const formData = new FormData();
+      formData.append("title", assignmentData.title);
+      formData.append("description", assignmentData.description);
+      formData.append("due_date", assignmentData.due_date);
+      formData.append("course_id", assignmentData.course_id);
+      if (assignmentData.attachment) {
+        formData.append("attachment", assignmentData.attachment);
+      }
+
+      const response = await api.post(`/assignments`, formData);
+      console.log("Added Assignment:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error adding assignment:", error.response.data);
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
