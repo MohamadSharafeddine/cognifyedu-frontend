0755f6c1df640534137755490dbacd764e747d89
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-11 17:27:31 +0300

    Created LoginUser in authSlice to login and save user data and token in local storage

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -14,3 +14,17 @@ export const registerUser = createAsyncThunk(
     }
   }
 );
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/login", credentials);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      localStorage.setItem("token", response.data.token);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
