0a281a775a1b4b3eaa7b137c12e0cf2ed517c7c1
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-11 22:20:45 +0300

    Added assignmentsReducer to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,12 +3,14 @@ import coursesReducer from "./slices/coursesSlice";
 import studentsReducer from "./slices/studentsSlice";
 import uiReducer from "./slices/uiSlice";
 import authReducer from "./slices/authSlice";
+import assignmentsReducer from "./slices/assignmentsSlice";
 
 const rootReducer = combineReducers({
   courses: coursesReducer,
   students: studentsReducer,
   ui: uiReducer,
   auth: authReducer,
+  assignments: assignmentsReducer,
 });
 
 export default rootReducer;
