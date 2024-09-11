9951cf4a51c857e4a4469693a84e8b8fda6d6a93
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-11 17:36:06 +0300

    Added new reducers to rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,14 @@
-import { combineReducers } from 'redux';
-import classesReducer from './slices/classesSlice';
-import studentsReducer from './slices/studentsSlice';
+import { combineReducers } from "@reduxjs/toolkit";
+import coursesReducer from "./slices/coursesSlice";
+import studentsReducer from "./slices/studentsSlice";
+import uiReducer from "./slices/uiSlice";
+import authReducer from "./slices/authSlice";
 
 const rootReducer = combineReducers({
-  classes: classesReducer,
+  courses: coursesReducer,
   students: studentsReducer,
+  ui: uiReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
