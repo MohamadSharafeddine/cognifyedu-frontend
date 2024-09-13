a6addb053c827732663ca15992db60f1888a3ff2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-13 14:27:42 +0300

    Updated DashboardStudent to load student specific components

--- src/pages/Dashboard/DashboardStudent.jsx
+++ src/pages/Dashboard/DashboardStudent.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import SubmitAssignmentPopup from "../../components/SubmitAssignmentPopup/SubmitAssignmentPopup";
 
 const DashboardStudent = () => {
   const { courses } = useSelector((state) => state.courses);
+  const [selectedAssignment, setSelectedAssignment] = useState(null);
+
+  const handleAssignmentClick = (assignment) => {
+    setSelectedAssignment(assignment); 
+  };
+
+  const closeSubmitAssignmentPopup = () => {
+    setSelectedAssignment(null); 
+  };
 
   return (
     <div className="dashboard">
@@ -12,7 +22,7 @@ const DashboardStudent = () => {
         <ul>
           {courses.map((courseItem) =>
             courseItem.assignments.map((assignment, index) => (
-              <li key={index}>
+              <li key={index} onClick={() => handleAssignmentClick(assignment)}>
                 <h4>{assignment.title}</h4>
                 <p>Due Date: {assignment.dueDate}</p>
                 <p>{assignment.submitted ? "Submitted" : "Not Submitted"}</p>
@@ -21,6 +31,13 @@ const DashboardStudent = () => {
           )}
         </ul>
       </div>
+
+      {selectedAssignment && (
+        <SubmitAssignmentPopup
+          assignment={selectedAssignment}
+          onClose={closeSubmitAssignmentPopup}
+        />
+      )}
     </div>
   );
 };
