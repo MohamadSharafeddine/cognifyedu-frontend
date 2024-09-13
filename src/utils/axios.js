a81bb839cf92a81142ddb50744d13549c1c1a6c2
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-13 14:25:52 +0300

    Updated axios to send token in the request to check authentication

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,32 +1,26 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('token');
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    baseURL: 'http://127.0.0.1:8000/api',
+    headers: {
+        'Authorization': token ? `Bearer ${token}` : '',
+        'Content-Type': 'application/json',
+    }
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    console.error('Error attaching token to request', error);
-    return Promise.reject(error);
-  }
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error('API response error:', error.response);
-    return Promise.reject(error);
-  }
 );
 
 export default api;
