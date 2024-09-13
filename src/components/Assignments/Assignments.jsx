2472727652744c48f844508e753f268338a7f932
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-13 14:36:52 +0300

    Updated Assignment to have different experience between user types

--- src/components/Assignments/Assignments.jsx
+++ src/components/Assignments/Assignments.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import "./Assignments.css";
 import Button from "../Button/Button";
 import EditAssignmentPopup from "../EditAssignmentPopup/EditAssignmentPopup";
 import DeleteConfirmationPopup from "../DeleteConfirmationPopup/DeleteConfirmationPopup";
 import ViewSubmissionsPopup from "../ViewSubmissionsPopup/ViewSubmissionsPopup";
-import { useDispatch } from "react-redux";
+import SubmitAssignmentPopup from "../SubmitAssignmentPopup/SubmitAssignmentPopup";
 import { deleteAssignment } from "../../redux/slices/assignmentsSlice";
 import moment from "moment";
 
 const Assignments = () => {
-  const { searchTerm, assignments } = useOutletContext();
+  const { searchTerm, assignments, user } = useOutletContext();
   const [selectedAssignment, setSelectedAssignment] = useState(null);
   const [showEditPopup, setShowEditPopup] = useState(false);
   const [showDeletePopup, setShowDeletePopup] = useState(false);
-  const [showSubmissionsPopup, setShowSubmissionsPopup] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("User type:", user?.type);
+    console.log("Assignments data:", assignments);
+  }, [user, assignments]);
 
   const filteredAssignments = assignments.filter((assignment) =>
     assignment.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -38,7 +44,7 @@ const Assignments = () => {
 
   const handleRowClick = (assignment) => {
     setSelectedAssignment(assignment);
-    setShowSubmissionsPopup(true);
+    setShowPopup(true);
   };
 
   return (
@@ -48,32 +54,38 @@ const Assignments = () => {
           <tr>
             <th>Title</th>
             <th>Due Date</th>
-            <th>Action</th>
+            {user?.type === "teacher" && <th>Action</th>}
           </tr>
         </thead>
         <tbody>
           {filteredAssignments.map((assignment, index) => (
-            <tr key={index} onClick={() => handleRowClick(assignment)} className="assignment-row">
+            <tr
+              key={index}
+              onClick={() => handleRowClick(assignment)}
+              className="assignment-row"
+            >
               <td>{assignment.title}</td>
-              <td>{moment(assignment.due_date).format('MMMM Do YYYY')}</td>
-              <td
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <Button
-                  color="#25738b"
-                  text="Edit"
-                  size="small"
-                  onClick={() => handleEditClick(assignment)}
-                />
-                <Button
-                  color="#e74c3c"
-                  text="Remove"
-                  size="small"
-                  onClick={() => handleDeleteClick(assignment)}
-                />
-              </td>
+              <td>{moment(assignment.due_date).format("MMMM Do YYYY")}</td>
+              {user?.type === "teacher" && (
+                <td
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <Button
+                    color="#25738b"
+                    text="Edit"
+                    size="small"
+                    onClick={() => handleEditClick(assignment)}
+                  />
+                  <Button
+                    color="#e74c3c"
+                    text="Remove"
+                    size="small"
+                    onClick={() => handleDeleteClick(assignment)}
+                  />
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
@@ -83,9 +95,7 @@ const Assignments = () => {
         <EditAssignmentPopup
           assignment={selectedAssignment}
           onClose={() => setShowEditPopup(false)}
-          onSave={(updatedAssignment) => {
-            setShowEditPopup(false);
-          }}
+          onSave={() => setShowEditPopup(false)}
         />
       )}
 
@@ -96,12 +106,20 @@ const Assignments = () => {
         />
       )}
 
-      {showSubmissionsPopup && (
-        <ViewSubmissionsPopup
-          assignmentTitle={selectedAssignment.title}
-          onClose={() => setShowSubmissionsPopup(false)}
-        />
-      )}
+      {showPopup &&
+        selectedAssignment &&
+        (user?.type === "teacher" ? (
+          <ViewSubmissionsPopup
+            assignmentTitle={selectedAssignment.title}
+            assignmentId={selectedAssignment.id}
+            onClose={() => setShowPopup(false)}
+          />
+        ) : (
+          <SubmitAssignmentPopup
+            assignment={selectedAssignment}
+            onClose={() => setShowPopup(false)}
+          />
+        ))}
     </div>
   );
 };
