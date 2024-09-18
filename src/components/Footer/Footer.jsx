8c91a4a7fbb23b0f77596d20ba70a149676b4fdf
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-09-18 03:43:07 +0300

    Added links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXTwitter, faInstagram, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom'; 
 import Logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="left">
-        <img src={Logo} alt="CognifyEdu Logo" />
+      <div className="footer-left">
+        <img src={Logo} alt="CognifyEdu Logo" className="footer-logo" />
         <div className="social-icons">
           <a href="https://x.com/i/flow/login" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faXTwitter} className="social-icon" />
@@ -25,15 +26,14 @@ const Footer = () => {
         </div>
         <div className="location">Lebanon - English</div>
       </div>
-      <div className="center">
-        <div>Support</div>
-        <a href="#">FAQ</a>
+      <div className="footer-center">
+        <div className="footer-title">Support</div>
+        <Link to="/faq" className="footer-link">FAQ</Link>
         <div className="copyright">© 2024 CognifyEdu</div>
       </div>
-      <div className="right">
-        <div>About</div>
-        <a href="#">Careers</a>
-        <a href="#">Terms & Conditions</a>
+      <div className="footer-right">
+        <div className="footer-title">About</div>
+        <Link to="/terms" className="footer-link">Terms & Conditions</Link>
       </div>
     </div>
   );
